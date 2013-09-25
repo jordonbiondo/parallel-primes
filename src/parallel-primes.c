b0abbd2005bedc4a2d0eb302bb729261981be535
jordonbiondo <biondoj@mail.gvsu.edu>
2013-09-25 11:48:55 -0400

    handle user input

--- src/parallel-primes.c
+++ src/parallel-primes.c
@@ -35,8 +35,27 @@ int child_getter(void) {
  * Main
  */
 int main(int argc, char* argv[]) {
+  
+  // modes
+  int prime_count_max = -1;
+  int prime_limit = -1;
+  
 
-  if (argc != 3) {
+  if (argc == 3) {
+    if (strcmp("-n", argv[1]) == 0) {
+      prime_count_max = (int)strtol(argv[2], NULL, 10);
+      if (prime_count_max <= 0) exit(-1);
+    } else if (strcmp("-limit", argv[1]) == 0) {
+      prime_limit = (int)strtol(argv[2], NULL, 10);
+      if (prime_limit <= 2) exit(-1);
+    } else {
+      goto badargs;
+    }
+  } else {
+    goto badargs;
+  }
+  
+  target(badargs) {
     printf("%s\n%s\n%s\n",
 	   "usage: parallel-primes [-n|-limit] [x | x > 0]",
 	   "\t-n x:\t\t print x number of primes.",
@@ -47,21 +66,24 @@ int main(int argc, char* argv[]) {
   // Number getting function
   number_getter next_num_func = parent_getter;
 
-  // modes
-  int prime_count_limit = 10;
 
   // initial prime
   int my_prime = 2;
   int prime_count = 0;
 
+  pid_t child_pid;
+  pipe_t proc_pipe;
 
+  
   /**
    * Child entry poing
    */
   {start_point: ;}
 
-  pid_t child_pid;
-  pipe_t proc_pipe;
+  // reset pid and pipe for each child
+  child_pid = 0;
+  PIPE_IN(proc_pipe) = 0;
+  PIPE_OUT(proc_pipe) = 0;
   pipe(proc_pipe);
 
   // fork appropriately
@@ -69,6 +91,8 @@ int main(int argc, char* argv[]) {
 
   // parent
   target(parent) {
+    printf("%d\n", my_prime);
+    fflush(stdout);
     dup2(PIPE_OUT(proc_pipe), STDOUT_FILENO);
     if (PIPE_CLOSE(proc_pipe) != 0) panic("pipe fail");
     int n = 1;
@@ -86,12 +110,11 @@ int main(int argc, char* argv[]) {
     if (PIPE_CLOSE(proc_pipe) != 0) panic("pipe fail");
 
     read(STDIN_FILENO, &my_prime, sizeof(int));
-
-    printf("%d\n", my_prime);
-    fflush(stdout);
     prime_count++;
     next_num_func = child_getter;
-    if (prime_count < prime_count_limit) goto start_point;
+    
+    if ((prime_count_max > 0 && prime_count < prime_count_max) || 
+	(prime_limit > 0 && my_prime < prime_limit)) goto start_point;
     else return 0;
   }
 
